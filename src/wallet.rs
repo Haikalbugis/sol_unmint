0b60f48ca4a310271f68aecc370c63c8a81f0e6a
haikal ubuntu <airdrop0599@gmail.com>
2025-10-10 17:08:31 +0800

    add generate new wallet

--- src/wallet.rs
+++ src/wallet.rs
@@ -27,3 +27,24 @@ impl Wallet {
         self.keypair.pubkey().to_string()
     }
 }
+
+pub struct Generate {
+    pub arry_private_key: [u8; 64],
+    pub base64_private_key: String,
+    pub address: String,
+}
+
+//generate new wallet
+pub fn generate() -> Generate {
+    let kp = Keypair::new();
+
+    let arry_private_key: [u8; 64] = kp.to_bytes();
+    let base64_private_key = kp.to_base58_string();
+    let address = kp.pubkey();
+
+    Generate {
+        arry_private_key,
+        base64_private_key,
+        address: address.to_string(),
+    }
+}
